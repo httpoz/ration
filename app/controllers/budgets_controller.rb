da666431143512dbbfe9ae1b54db1a806a89397f
Ogge Norin <oskar_norin@hotmail.com>
2017-05-09 18:10:35 +0000

    Added budgets table, model, controller with new, create and show action and routes and views

---
+++ app/controllers/budgets_controller.rb
@@ -0,0 +1,30 @@
+class BudgetsController < ApplicationController 
+  
+  def new
+    @budget = Budget.new
+  end
+  
+  def create
+    # This will instead be build through User association
+    # With User.budgets.build
+    @budget = Budget.new( budget_params )
+    if @budget.save
+      # Should redirect to User where budget can be displayed
+      redirect_to @budget
+    else
+      # Error message should display¨
+      render :new
+    end
+  end
+  
+  # Will be removed since budget will be showed through user
+  def show
+    @budget = Budget.find( params[:id] )
+  end
+  
+  private
+    def budget_params
+      params.require(:budget).permit(:name)
+    end
+  
+end
---
+++ app/models/budget.rb
@@ -0,0 +1,5 @@
+class Budget < ApplicationRecord
+  
+  
+  
+end
