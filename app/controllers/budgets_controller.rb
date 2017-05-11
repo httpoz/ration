e049051bdfce494cfb4006b8cebd96320ba23fd9
Oscar Mwanandimai <oscar.mwana@gmail.com>
2017-05-11 08:10:04 +0200

    :hammer: budgets crud

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,7 +1,12 @@
 class BudgetsController < ApplicationController
+  before_action :set_budget, only: [:show, :edit, :update, :destroy]
 
   def index
-    @budgets = Budget.all
+    @budgets = current_user.budgets.all
+  end
+
+  # GET /budgets/1/edit
+  def edit
   end
 
   def new
@@ -11,13 +16,37 @@ class BudgetsController < ApplicationController
   def create
     # This will instead be build through User association
     # With User.budgets.build
-    @budget = Budget.new( budget_params )
+    @budget = current_user.budgets.new( budget_params )
     if @budget.save
       # Should redirect to User where budget can be displayed
       redirect_to @budget
     else
       # Error message should display¨
       render :new
+    end
+  end
+
+  # PATCH/PUT /budgets/1
+  # PATCH/PUT /budgets/1.json
+  def update
+    respond_to do |format|
+      if @budget.update(budget_params)
+        format.html { redirect_to @budget, notice: 'Budget was successfully updated.' }
+        format.json { render :show, status: :ok, location: @budget }
+      else
+        format.html { render :edit }
+        format.json { render json: @budget.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /budgets/1
+  # DELETE /budgets/1.json
+  def destroy
+    @budget.destroy
+    respond_to do |format|
+      format.html { redirect_to budgets_url, notice: 'Budget was successfully destroyed.' }
+      format.json { head :no_content }
     end
   end
 
@@ -29,8 +58,11 @@ class BudgetsController < ApplicationController
   end
 
   private
-    def budget_params
-      params.require(:budget).permit(:name)
+    def set_budget
+      @budget = Budget.find(params[:id])
     end
+  def budget_params
+    params.require(:budget).permit(:name)
+  end
 
 end
