d1e87e23e9b95d9d4be516cc8af5deba3f99d081
Ogge Norin <oskar_norin@hotmail.com>
2017-05-18 20:19:54 +0000

    Added total expense amount and small styling to budget show

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -1,2 +1,11 @@
 module BudgetsHelper
+ 
+  def calculate_total(category)
+    sum = 0
+    @budget.expenses.each do |expense|
+      sum += expense.amount if category == expense.category
+    end
+    sum
+  end
+  
 end
