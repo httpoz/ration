b0dbd276824d481dbf44f80996244679593748e9
Oscar Mwanandimai <oscar.mwana@gmail.com>
2017-07-21 17:24:14 +0200

    lazy wip

--- app/assets/javascripts/components/_profile.js.jsx
+++ app/assets/javascripts/components/_profile.js.jsx
@@ -1,7 +1,17 @@
 const Profile = React.createClass({
     render() {
         return ( <div>
-            <h1>Hello, World!</h1>
+            <form>
+                <div className="form-group">
+                    <input type="text" name="current_password" className="form-control" placeholder="Current Password"/>
+                </div>
+                <div className="form-group">
+                    <input type="text" name="new_password" className="form-control" placeholder="New Password"/>
+                </div>
+                <div className="form-group">
+                    <input type="text" name="confirm_password" className="form-control" placeholder="Confirm Password"/>
+                </div>
+            </form>
         </div> )
     }
 });
