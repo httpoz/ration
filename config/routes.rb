67ecb93f94dfaf1e390b7eaca3928e9a84c5204f
Oscar Mwanandimai <oscar.mwana@gmail.com>
2021-02-08 14:33:07 +0100

    run rails update

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  unauthenticated do
-    root 'static_pages#home'
-  end
-
-  authenticated do
-    root 'dashboard#index'
-    resources :profile, only: [:index, :store]
-  end
-
-  resources :categories
-  resources :budgets do
-    resources :expenses
-  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
