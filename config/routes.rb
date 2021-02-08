7707879f1c7721116db02ffb61dd3bd0e8a6b1f9
Oscar Mwanandimai <oscar.mwana@gmail.com>
2021-02-08 15:20:06 +0100

    restore routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  unauthenticated do
+    root 'static_pages#home'
+  end
+
+  authenticated do
+    root 'dashboard#index'
+    resources :profile, only: [:index, :store]
+  end
+
+  resources :categories
+  resources :budgets do
+    resources :expenses
+  end
 end
